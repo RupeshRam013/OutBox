c87bbd09dcd852f69149bcda7d41dbad26937d42
Rupesh Ram <94728392+rupeshram013@users.noreply.github.com>
2023-06-07 13:29:04 +0545

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./Objects/scripts/navbar.js";
 import Intro from "./Objects/scripts/intro.js";
 import Items from "./Objects/scripts/items.js";
+import Form from "./Objects/scripts/form.js";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Navbar/>
       <Intro/>
       <Items/>
+      <Form/>
    </div> 
   );
 }
